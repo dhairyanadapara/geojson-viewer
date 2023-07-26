75a8f6bc5504d24e3117f11c26b1bfd81422f61a
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2023-07-27 00:03:13 +0530

    large json fix

--- src/components/map_component.tsx
+++ src/components/map_component.tsx
@@ -1,21 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import 'ol/ol.css';
 
 import useMap from './useMap';
 import { MinusIcon, AddIcon, EditIcon, SearchIcon } from '@chakra-ui/icons'
 import { GeoJSONData } from '../types';
 import { IconButton } from '@chakra-ui/react';
-import useGeoJson from '../useGeoJson';
+// import useGeoJson from '../useGeoJson';
 
 const MapComponent = () => {
     const {
         addLayer, handleZoomIn, handleZoomOut,
     } = useMap();
-    const {
-        clusterGeoJSON,
-    } = useGeoJson();
+
     const fileInputRef = useRef<HTMLInputElement>(null);
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,10 +24,6 @@ const MapComponent = () => {
             reader.onload = (e) => {
                 try {
                     const geojsonData = JSON.parse(e.target?.result as string) as GeoJSONData;
-                    if (file?.size > 2 * 1048 * 1048) {
-                        const data = clusterGeoJSON(geojsonData)
-                        console.log(data)
-                    }
                     addLayer(geojsonData, 1);
                 }
                 catch (error) {
