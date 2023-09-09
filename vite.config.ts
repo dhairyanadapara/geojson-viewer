b0dbc04122c40c9e1858222778a07a42aed71a11
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2023-09-10 00:07:59 +0530

    fix: updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": process.env,
+    VITE_API_SERVER_URL: process.env.VITE_API_SERVER_URL,
+  },
 })
